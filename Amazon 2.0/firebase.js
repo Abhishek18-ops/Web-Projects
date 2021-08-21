fd2135ef446ece70146e60c1873479b3292e0ef6
Abhishek Maurya <abhisheaurya@gmail.com>
2021-08-21 22:26:06 +0530

    Cart function

---
+++ Amazon 2.0/firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyBFJVhPkXEzyZ0Jjr5PIwMoFnvK7FdgBik",
+    authDomain: "test-b6e09.firebaseapp.com",
+    projectId: "test-b6e09",
+    storageBucket: "test-b6e09.appspot.com",
+    messagingSenderId: "235517299255",
+    appId: "1:235517299255:web:dd25f64cab6374a6ca8db2",
+    measurementId: "G-VRJKMT7B27"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
